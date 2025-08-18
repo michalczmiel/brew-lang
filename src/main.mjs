f02f6505f664d67b2e0eb48382b2c720942c94b9
Michał Czmiel <michalczmiel@outlook.com>
2025-08-18 18:25:40 +0200

    feat: add vim mode to the editor

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,6 @@
 import { basicSetup } from "codemirror";
 import { EditorView } from "@codemirror/view";
+import { vim } from "@replit/codemirror-vim";
 
 import { grammar } from "./grammar.mjs";
 import { glitchCoffeeOrigamiHot } from "./recipes.mjs";
@@ -8,6 +9,7 @@ import { highlighting } from "./highlighting.mjs";
 window.addEventListener("DOMContentLoaded", () => {
   const editorContainer = document.getElementById("editor");
   const consoleContainer = document.getElementById("console");
+  const vimToggle = document.getElementById("vim-toggle");
 
   const updateListener = EditorView.updateListener.of((update) => {
     if (update.docChanged) {
@@ -21,9 +23,30 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  new EditorView({
-    doc: glitchCoffeeOrigamiHot,
-    parent: editorContainer,
-    extensions: [basicSetup, highlighting, updateListener],
+  let editor;
+
+  function createEditor({ useVim, doc }) {
+    const extensions = [basicSetup, highlighting, updateListener];
+    if (useVim) {
+      extensions.unshift(vim());
+    }
+
+    return new EditorView({
+      doc,
+      parent: editorContainer,
+      extensions,
+    });
+  }
+
+  editor = createEditor({ useVim: false, doc: glitchCoffeeOrigamiHot });
+
+  vimToggle.addEventListener("change", (event) => {
+    const currentDoc = editor.state.doc.toString();
+    editor.destroy();
+
+    editor = createEditor({ useVim: event.target.checked, doc: currentDoc });
+    editor.dispatch({
+      changes: { from: 0, to: editor.state.doc.length, insert: currentDoc },
+    });
   });
 });
