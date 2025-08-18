83cff591bf157a5f091da0785d17023fd46090ae
Michał Czmiel <michalczmiel@outlook.com>
2025-08-18 18:59:48 +0200

    refactor: add time_instruction construct

--- src/grammar.mjs
+++ src/grammar.mjs
@@ -5,16 +5,14 @@ export const grammar = ohm.grammar(String.raw`
     recipe = (method | step | temperature | water | dose | comment | newline)*
 
     method = "method" " " (~newline any)+ newline?
-    dose = "dose" " " number
+    dose = "dose" " " number newline?
     temperature = "temperature" " " (range | number)+ newline?
     water = "water" " " number newline?
 
     step = "step" " " "{" newline? instruction+ " "* "}"
-    instruction = " "* (start | finish | pour | duration | comment) newline
+    instruction = " "* (time_instruction | pour | comment) newline
 
-    duration = "duration" " " duration_number
-    finish = "finish" " " duration_number
-    start = "start" " " duration_number
+    time_instruction = ("duration" | "start" | "finish") " " duration_number
     pour = "pour" " " number
 
     comment = "#" (~newline any)*
